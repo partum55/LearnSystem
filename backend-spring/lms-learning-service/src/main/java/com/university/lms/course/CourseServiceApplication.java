dc295461e6c7be802caac06902ed51c8976d782b
partum55 <partumyt@gmail.com>
2026-02-22 18:55:43 +0200

    fix: add missing annotations for JPA repositories and entity scanning

--- backend-spring/lms-learning-service/src/main/java/com/university/lms/course/CourseServiceApplication.java
+++ backend-spring/lms-learning-service/src/main/java/com/university/lms/course/CourseServiceApplication.java
@@ -2,9 +2,11 @@ package com.university.lms.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -17,6 +19,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
       "com.university.lms.deadline",
       "com.university.lms.common"
     })
+@EnableJpaRepositories(basePackages = "com.university.lms")
+@EntityScan(basePackages = "com.university.lms")
 @EnableJpaAuditing
 @EnableCaching
 @EnableScheduling
